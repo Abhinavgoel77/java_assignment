e94ae729632580db0d68b55a6a6249e319f05986
Abhinav goel <Abhinavgoel77@gmail.com>
2021-08-29 16:24:08 +0530

    java assignment

---
+++ core java/1st.java
@@ -0,0 +1,54 @@
+
+// question : Implement your  own abstract  classes and  interfaces
+
+// =========================================================================================
+//abstract class
+===========================================================================================
+abstract class Base {
+    abstract void fun();             // object of abstract class cant be created
+}
+class Derived extends Base {
+    void fun()
+    {
+        System.out.println("Derived fun() called");
+    }
+}
+class Main {
+    public static void main(String args[])
+    {
+  
+        Base b = new Derived();
+        b.fun();
+    }
+}
+
+===============================================================================================
+// interfaces
+===========================================================================================
+
+import java.io.*;
+  
+interface In1
+{
+     int a = 10;
+    void display();
+}
+  
+// A class that implements the interface.
+class TestClass implements In1
+{
+    // Implementing the capabilities of
+    // interface.
+    public void display()
+    {
+        System.out.println("Geek");
+    }
+  
+    // Driver Code
+    public static void main (String[] args)
+    {
+        TestClass t = new TestClass();
+        t.display();
+        System.out.println(a);
+    }
+}
---
+++ core java/3rd.java
@@ -0,0 +1,65 @@
+// QUESTION -  Create a thread named  fetch Data using thread  extend methodFetch data should  implement sleep method 
+//             with 5000ms timeCreate a thread named  processData  using  runnable  interfaceMake sure processData 
+//              starts its execution only after fetchData thread has  completed its execution with the timeout of 10000ms.
+
+// ============================================================================================================================
+
+import java.util.*;
+import java.lang.*;
+
+class extendThread extends Thread
+{
+    public void sleep(){
+        try{
+            Thread.sleep(5000);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    public void run(){
+        System.out.println("Thread is implementing with Thread class.");
+    }
+}
+
+class implementsThread implements Runnable
+{
+    public void sleep(){
+        try{
+            Thread.sleep(10000);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    public void run(){
+        System.out.println("Thread is implementing with Runnable interface.");
+    }
+}
+
+
+class ThreadSleep
+{
+    public static void main(String[] args){
+       
+        extendThread fetchData = new extendThread();
+       
+        implementsThread processData = new implementsThread();
+       
+        System.out.println("Threads created!");
+        
+        System.out.println("fetchData thread is undergoing a sleep of 5000 ms.");
+        fetchData.sleep();
+        fetchData.run();
+        System.out.println("fetchData thread completed a sleep of 5000 ms.");
+
+        System.out.println("processData thread is undergoing a sleep of 10000 ms.");
+        processData.sleep();
+        processData.run();
+        System.out.println("processData thread completed a sleep of 10000 ms.");
+        
+        System.out.println("Execution Finished!");
+    }
+}
